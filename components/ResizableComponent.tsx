2fdb9547d80766f6c3d767177b55c0a236c0a69e
khoind4 <khoind4@vng.com.vn>
2023-02-20 14:11:30 +0700

    fix: fix bug layout

--- components/ResizableComponent.tsx
+++ components/ResizableComponent.tsx
@@ -5,7 +5,7 @@ type TResizableComponentProps = {
 
 export function ResizableComponent({ children, className }: TResizableComponentProps) {
   return (
-    <div className={`${className} resize-x overflow-x-auto`} draggable>
+    <div className={`${className} resize-x overflow-x-hidden overflow-y-hidden h-screen`} draggable>
       {children}
     </div>
   );
